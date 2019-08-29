77ea5447effb342c277b88ab3da564bcbf069521
jiangxin <1532256531@qq.com>
2019-08-29 17:42:00 +0800

    新增司机路线页面，我的车辆页面，上传tabBar图片一张

---
+++ src/components/VehicleLine.vue
@@ -0,0 +1,84 @@
+<template>
+	<!-- 司机路线通用组件 -->
+	<div class="vehicleLine">
+		<div>
+			<p>
+				<span>始发地</span>
+				<span style="color: #7E8C8D;">→</span>
+				<span>目的地</span>
+				<span>运费</span>
+			</p>
+			<p>
+				<span>刚刚&nbsp;&nbsp;</span>
+				<span>发布</span>
+			</p>
+		</div>
+		<div>
+			<p>
+				<span>货物名称&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+				<span>货物重量/体积</span>
+			</p>
+			<p>
+				<span>15:00</span>
+				<span>装货</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				msg: 'Line页面'
+			}
+		},
+		created() {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.vehicleLine {
+		padding: 15px;
+		font-size: 16Px;
+		color: #666666;
+
+		div {
+			display: flex;
+			justify-content: space-between;
+		}
+
+		div:first-child {
+			margin-bottom: 15px;
+
+			p:first-child {
+				span {
+					color: #303030;
+					font-weight: 600;
+					margin-right: 10px;
+				}
+
+				span:last-child {
+					color: #FF5252;
+					font-size: 12Px;
+					font-weight: 400;
+				}
+			}
+
+			p:last-child {
+				color: #999999;
+				font-size: 14Px;
+			}
+		}
+
+		div:last-child {
+			font-size: 14Px;
+
+			p:last-child {
+				color: #999999;
+			}
+		}
+	}
+</style>
---
+++ src/views/my_vehicle.vue
@@ -0,0 +1,97 @@
+<template>
+	<!-- 我的车辆 -->
+	<div class="my-vehicle">
+		<van-nav-bar border title="司机资料" right-text="添加车辆" left-arrow fixed @click-right="onClickRight" @click-left="onClickLeft" />
+		<!-- 搜索框 -->
+		<div class="box">
+			<div class="search">
+				<van-search placeholder="输入名字搜索车辆" v-model="value" />
+			</div>
+			<div class="box-1">
+				<div class="vehicle-1 vehicle-0">
+					<p>
+						<img src="@/assets/img/vehicle/my_vehicle.png" alt="">
+						<span>司机车牌号</span>
+						<span>车长 | 车型</span>
+					</p>
+					<van-button type="default" round plain color="#F9A825"  size="mini">下班</van-button>
+				</div>
+				<div class="vehicle-2 vehicle-0">
+					<p>
+						<img src="@/assets/img/vehicle/my_vehicle.png" alt="">
+						<span>司机车牌号</span>
+						<span>车长 | 车型</span>
+					</p>
+					<van-button type="default" round color="#F9A825" size="mini">上班</van-button>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				value: ''
+			}
+		},
+		methods: {
+			onClickLeft() {
+				this.$router.go(-1);
+			},
+			onClickRight() {
+				console.log('添加车辆事件');
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	// vant样式修改
+	.van-nav-bar__text {
+		color: #F9A825;
+	}
+	.van-button {
+		height: 18Px;
+		padding: 0px 14px;
+		line-height: 18Px;
+	}
+	.box {
+		margin-top: 46Px;
+	}
+	.search {
+		padding: 6px;
+		background-color: #ffffff;
+		.van-search__content {
+			border-radius: 17Px;
+			background-color: #EEEEEE;
+		}
+	}
+	.box-1 {
+		padding: 0 15px;
+		.vehicle-0 {
+			display: flex;
+			padding: 10px 16px;
+			justify-content: space-between;
+			margin-top: 20px;
+			background-color: #ffffff;
+			align-items: center;
+			p {
+				font-size: 14Px;
+				* {
+					margin-right: 10px;
+					color: #303030;
+				}
+				span:last-child {
+					color: #999999;
+					font-size: 12Px;
+				}
+				img {
+					width: 25Px;
+					vertical-align: middle;
+				}
+			}
+		}
+	}
+</style>
